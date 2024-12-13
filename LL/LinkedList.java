0642a03767fd1283b8424ed286530fed5dc569e2
Your Name <jagandeesh02@gmail.com>
2024-12-13 08:48:59 +0530

    Insert

--- LL/LinkedList.java
+++ LL/LinkedList.java
@@ -14,13 +14,72 @@ public class LinkedList{
 		}		
 	}	
 	public void insertFirst(int data){
+
 		Node n=new Node(data);
 		n.next=head;
 		head=n;
 		if(tail==null){
 			tail=head;
 		}
+				size++;
+		
+	}
+	public void insertLast(int data){
+		
+		if(tail==null){
+			insertFirst(data);
+					size++;
+			return;
+		}
+		Node n=new Node(data);
+		tail.next=n;
+		tail=n;
+				size++;
+		
+	}
+	public void deleteAtIndex(int index){
+		
+		if(index==0){
+			head=head.next;
+			size--;
+			return;
+		}
+		if(index==size){
+			tail=null;
+			size--;
+			return;
+			
+		}
+		Node temp=head;
+		for(int i=1;i<index;i++){
+			temp=temp.next;
+		}
+		temp.next=temp.next.next;
+		size--;
+		
+	}
+	public void insert(int data,int index){
+		
+		if(index==0){
+			insertFirst(data);
+			size++;
+			return;
+		}
+		if(index==size){
+			insertLast(data);
+			size++;
+			return;
+		}
+		Node temp=head;
+		for(int i=1;i<index;i++){
+			temp=temp.next;
+		}
+		Node n=new Node(data,temp.next);
+		temp.next=n;
 		size++;
+	}
+	public int len(){
+		return size;
 	}
 	public void print(){
 		Node temp=head;
--- LL/Main.java
+++ LL/Main.java
@@ -1,10 +1,18 @@
 public class Main{
 	public static void main(String args[]){
 		LinkedList ll=new LinkedList();
-		ll.insertFirst(10);
-		ll.insertFirst(20);
-		ll.insertFirst(30);
-		ll.insertFirst(40);
+		ll.insertLast(10);
+		ll.insertLast(20);
+		ll.insertLast(30);
+		ll.insertLast(40);
+		ll.insertLast(1);
+		ll.insert(3,4);
 		ll.print();
+		
+		System.out.println(ll.len());
+		ll.deleteAtIndex(ll.len()-2);
+		
+		ll.print();
+		System.out.println(ll.len());
 	}
 }
